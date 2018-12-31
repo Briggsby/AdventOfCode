2b2d442f39552d45b849d5fd4b65cc60bdba4cf7
Briggsby <jackbriggsby@gmail.com>
2018-12-30 21:50:09 -0500

    Found probably bug but can't check easily. Networkx better anyway.

--- 2018_22_Take3.py
+++ 2018_22_Take3.py
@@ -1,3 +1,6 @@
+import networkx as nx
+
+
 def get_erosion(coords, cave_map, depth, target):
     x = coords[0]
     y = coords[1]
@@ -98,6 +101,9 @@ def shortest_path(start, target, depth, cave_map=None):
                     else:
                         open_list.append(path)
                 else:
+                    # HERE is the problem !!! (I think, this method is too slow to check nicely)
+                    # Checks to see if the path is on the list in a shorter form than current, but
+                    # needs to check if it's that location AND equipment that's faster
                     if path_map[path][g] > path_map[current][g] + distance(path, current):
                         open_list.remove(path)
                         path_map[path][parent] = current
@@ -122,12 +128,30 @@ def make_map(depth, target, x_size, y_size):
         cave_map[(0, y)] = ((y * 48271) + depth) % 20183
     for x in range(1, x_size):
         for y in range(1, y_size):
-            cave_map[(x, y)] = ((cave_map[(x-1, y)] * cave_map[(x, y-1)]) + depth) % 20183
+            if (x, y) == target:
+                cave_map[(x, y)] = depth % 20183
+            else:
+                cave_map[(x, y)] = ((cave_map[(x-1, y)] * cave_map[(x, y-1)]) + depth) % 20183
     return cave_map
 
 
-cave_map_prepped = make_map(510, (10, 10), 311, 1000)
-print(shortest_path((0, 0, 1), (10, 10, 1), 510))
-cave_map_prepped = make_map(11739, (11, 718), 311, 1000)
-print(shortest_path((0, 0, 1), (11, 718, 1), 11739))
+def make_nx_graph(depth, target, overshoot):
+    cave_map = make_map(depth, target, target[0]+overshoot, target[1]+overshoot)
+    graph = nx.Graph()
+    for x in range(target[0]+overshoot):
+        for y in range(target[1]+overshoot):
+            risk = cave_map[(x, y)] % 3
+            items = available_levels(risk)
+            graph.add_edge((x, y, items[0]), (x, y, items[1]), weight=7)
+            adjacent = ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
+            for i in adjacent:
+                if target[0]+overshoot > i[0] >= 0 and target[1]+overshoot > i[1] >= 0:
+                    for item in [j for j in items if j in available_levels(cave_map[i] % 3)]:
+                        graph.add_edge((x, y, item), (i[0], i[1], item), weight=1)
+    return graph
+
+
+nx_graph = make_nx_graph(11739, (11, 718), 200)
+print(nx.dijkstra_path_length(nx_graph, (0, 0, 1), (11, 718, 1)))
+print(nx.astar_path_length(nx_graph, (0, 0, 1), (11, 718, 1)))
 
